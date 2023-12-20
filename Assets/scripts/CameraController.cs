b0c5a5f5e8c5fe49447bee8009257c43ff101ac4
Ruby <77706906+QuartetBaguette@users.noreply.github.com>
2023-12-20 14:56:51 +0100

    added camera movement, updated assets & character jump sprite

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update() {
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void MoveToNewRoom(Transform _newRoom) {
+        currentPosX = _newRoom.position.x;
     }
 }
