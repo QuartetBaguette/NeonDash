d6f62ddf42ad0b9519ca4c50e18ce3df343181cc
Ruby <77706906+QuartetBaguette@users.noreply.github.com>
2024-01-15 13:48:33 +0100

    Added environmental damage

---
+++ Assets/scripts/health/DamagePlayer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().TakeDamage(1);
+        }
+    }
+}
---
+++ Assets/scripts/health/Health.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Scripting;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set;}
+    private Animator anim;
+    private bool dead;
+
+    private void Awake() {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float _damage) {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        
+        if(!dead) {
+            anim.SetTrigger("die");
+            GetComponent<PlayerMovement>().enabled = false;
+            dead = true;
+        }
+        // if(currentHealth > 0) {
+        //     //Player hurt
+        // } else {
+        //     //Player dead
+        // }
+    }
+}
